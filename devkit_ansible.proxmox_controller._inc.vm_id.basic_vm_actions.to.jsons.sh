2f11f8bb43173612154cd3de9b160bef3e2cd6ad
Benjamin Collas <@hyde-repo>
2025-06-03 01:32:16 +0200

    :::: major changes

--- devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.jsons.sh
+++ devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.jsons.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 showExample() {
-  echo ""
-  echo "$(basename "$0") VM_ID"
-  echo "$(basename "$0") 24242"
-  echo "$(basename "$0") 34242"
-  echo ""
+  echo
+  echo "VM_ID | $(basename "$0") vm_start"
+  echo "24242 | $(basename "$0") vm_stop"
+  echo "24242 | $(basename "$0") vm_stop_force"
+  echo "34242 | $(basename "$0")"
+  echo
 }
 
 if [ "$1" = '-h' ] ||
@@ -15,8 +16,8 @@ if [ "$1" = '-h' ] ||
   echo
   echo SYNOPSIS
   echo "  $(basename "$0") [-h|--help] [URL]"
-  echo "  $(basename "$0") [VM_ID] [--json] - force output as json "
-  echo "  $(basename "$0") [VM_ID] [--text] - force output as text"
+  echo "  echo [VM_ID] | $(basename "$0") [--json] - force output as json *default "
+  echo "  echo [VM_ID] | $(basename "$0") [--text] - force output as text"
   echo ""
   echo EXAMPLE
   echo "  $(showExample)"
@@ -31,43 +32,49 @@ DEFAULT_OUTPUT_JSON=true
 DEFAULT_OPEN_VAULT_PW_FILE_PATH="/tmp/vault/vault_pass.txt"
 
 ARG_ACTION="${1:-}"
-ARG_VM_ID="${2:-}"
+# ARG_VM_ID="${2:-}"
 
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
 # check if arguments provided
-if [[ -z "$ARG_ACTION" || -z "$ARG_VM_ID" ]]; then
+if [[ -z "$ARG_ACTION" ]]; then
+  # if [[ -z "$ARG_ACTION" || -z "$ARG_VM_ID" ]]; then
+
+  devkit_generic.utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
   showExample
   exit 1
 fi
 
-# quick dirty notes :
+# # quick dirty notes :
 
-# vm_create - multiple
+# # vm_create - multiple
 
-# firewall_dc_enable - 0
-# firewall_node_enable - 0
-# firewall_vm_enable - ok
+# # firewall_dc_enable - 0
+# # firewall_node_enable - 0
+# # firewall_vm_enable - ok
 
-# snapshot_create - ok - 2
-# snapshot_delete - ok - 2
-# snapshot_rollback - arg 2 ?
+# # snapshot_create - ok - 2
+# # snapshot_delete - ok - 2
+# # snapshot_rollback - arg 2 ?
 
-# vm_delete - ok
-# vm_pause - ok
-# vm_resume - ok
-# vm_start - ok
-# vm_stop - ok
-# vm_stop_force - ok
+# # vm_delete - ok
+# # vm_pause - ok
+# # vm_resume - ok
+# # vm_start - ok
+# # vm_stop - ok
+# # vm_stop_force - ok
 
 # white listing doc helper
 case "$ARG_ACTION" in
 vm_delete | vm_pause | vm_resume | vm_start | vm_stop | vm_stop_force | vm_list) ;;
 *)
-  echo ""
-  echo ":: ERROR   :: invalid action: $ARG_ACTION"
-  echo ":: ALLOWED :: start | stop | pause | reset | resume"
-  echo ""
+  # echo ""
+  # echo ":: ERROR   :: invalid action: $ARG_ACTION"
+  # echo ":: ALLOWED :: start | stop | pause | reset | resume"
+  devkit_generic.utils.text.echo_error.to.text.to.stderr.sh "invalid action - $ARG_ACTION"
+  devkit_generic.utils.text.echo_error.to.text.to.stderr.sh ":: ALLOWED :: start | stop | pause | reset | resume"
+  # echo ""
+  showExample
   exit 1
   ;;
 esac
@@ -84,7 +91,7 @@ devkit_ansible.proxmox_controller._inc.warmup_checks.sh
 
 OUTPUT_JSON="$DEFAULT_OUTPUT_JSON"
 
-case "${3:-}" in
+case "${2:-}" in
 --json)
   OUTPUT_JSON=true
   ;;
@@ -94,15 +101,16 @@ case "${3:-}" in
 
 "") ;;
 *)
-  echo ":: ERROR :: invalid argument.  '$2'." >&2
 
+  devkit_generic.utils.text.echo_error.to.text.to.stderr.sh "invalid argument '$2' "
+  exit 1
   ;;
 esac
 
 if [[ "$OUTPUT_JSON" == true ]]; then
   CURRENT_ANSIBLE_CONFIG="./ansible_no_skipped_json.cfg"
 else
-  # CURRENT_ANSIBLE_CONFIG="./ansible_no_skipped.cfg"
+  CURRENT_ANSIBLE_CONFIG="./ansible_no_skipped.cfg"
   CURRENT_ANSIBLE_CONFIG="./ansible.cfg"
 fi
 
@@ -127,13 +135,19 @@ fi
 #
 #### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
 
-(
-  # ANSIBLE_CONFIG="./ansible_no_skipped_json.cfg"
+IFS=$'\n'
 
-  ANSIBLE_CONFIG="$CURRENT_ANSIBLE_CONFIG" \
-    ansible-playbook -i "$RANGE42_ANSIBLE_ROLES__INVENTORY_DIR/off_cr_42.yaml" \
-    "${ANSIBLE_VAULT_ARG[@]}" \
-    /dev/stdin <<EOF
+for VM_ID in $(cat - | tr -d '[:space:]'); do
+
+  devkit_generic.utils.text.echo_error.to.text.to.stderr.sh "revceived $VM_ID"
+
+  (
+    # ANSIBLE_CONFIG="./ansible_no_skipped_json.cfg"
+
+    ANSIBLE_CONFIG="$CURRENT_ANSIBLE_CONFIG" \
+      ansible-playbook -i "$RANGE42_ANSIBLE_ROLES__INVENTORY_DIR/off_cr_42.yaml" \
+      "${ANSIBLE_VAULT_ARG[@]}" \
+      /dev/stdin <<EOF
 
 - hosts: px-testing
   gather_facts: false
@@ -145,7 +159,15 @@ fi
         name: $ROLE_NAME
       vars:
         proxmox_vm_action: "$ARG_ACTION"
-        proxmox_vmid: $ARG_VM_ID
+        proxmox_vmid: !!int $VM_ID
       
 EOF
-)
+  ) | jq --arg action "$ARG_ACTION" '
+         .plays[].tasks[] 
+        | .hosts[] 
+        | select(type=="object" and has($action))
+        | .[$action]
+      '
+  # jq -c '.[]'
+
+done
