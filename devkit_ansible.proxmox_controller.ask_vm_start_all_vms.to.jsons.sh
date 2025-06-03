3b6bd8b482225303556598594e7e45dbfad127ff
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-06-03 16:02:01 +0200

    :::: new minor feature

---
+++ devkit_ansible.proxmox_controller.ask_vm_start_all_vms.to.jsons.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+set -euo pipefail
+
+ACTION="vm_start"
+DEFAULT_OUTPUT_JSON=true
+ARG_VM_NAME_FILTER=""
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+showExample() {
+  echo
+  echo "$(basename "$0") "
+  echo
+}
+
+if [ "${1-}" = '-h' ] || [ "${1-}" = '--help' ]; then
+
+  echo NAME
+
+  echo "  $(basename "$0") - Start all vm - Execute the specified $ACTION action via Ansible (all vms) "
+  echo
+  echo SYNOPSIS
+  echo "  $(basename "$0") [-h|--help] "
+  echo "  $(basename "$0") [VM_ID] [--json] - force output as json "
+  echo "  $(basename "$0") [VM_ID] [--text] - force output as text"
+  echo ""
+  echo EXAMPLE
+  echo "  $(showExample)"
+  exit 1
+fi
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+devkit_ansible.proxmox_controller._inc.warmup_checks.sh
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#
+# define output type
+#
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+OUTPUT_JSON="$DEFAULT_OUTPUT_JSON"
+
+case "${1:-}" in
+--json)
+  OUTPUT_JSON=true
+  ;;
+--text)
+  OUTPUT_JSON=false
+  ;;
+"") ;;
+*)
+
+  if [[ -z "$ARG_VM_NAME_FILTER" ]]; then
+    ARG_VM_NAME_FILTER="$1"
+    shift
+  else
+    devkit_generic.utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+    showExample
+    exit 1
+  fi
+  ;;
+esac
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#
+# inc lib script call.
+#
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+# for VM_ID in $(devkit_ansible.proxmox_controller.ask_vm_list_running_and_extract_vm_id.to.text.sh); do
+
+#   if [[ "$OUTPUT_JSON" == true ]]; then
+
+#     (
+#       echo "$VM_ID" |
+#         devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.jsons.sh "$ACTION"
+#     )
+#   else
+#     (
+#       echo "$VM_ID" |
+#         devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.text.sh "$ACTION"
+#     )
+#   fi
+
+#   devkit_generic.utils.text.echo_pass.to.text.to.stderr.sh "stopping :: $VM_ID"
+#   sleep 7 # ACPI shutdown take few seconds...
+# done
+
+IFS=$'\n'
+
+for VM_ID in $(
+
+  if [[ -n "$ARG_VM_NAME_FILTER" ]]; then
+    devkit_ansible.proxmox_controller.ask_vm_list_stopped.to.jsons.sh "$ARG_VM_NAME_FILTER" |
+      jq -r '.vm_id'
+  else
+    devkit_ansible.proxmox_controller.ask_vm_list_stopped.to.jsons.sh |
+      jq -r '.vm_id'
+  fi
+
+); do
+
+  ####
+
+  if [[ "$OUTPUT_JSON" == true ]]; then
+    (
+      echo "$VM_ID" |
+        devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.jsons.sh "$ACTION"
+    )
+  else
+    (
+      echo "$VM_ID" |
+        devkit_ansible.proxmox_controller._inc.vm_id.basic_vm_actions.to.text.sh "$ACTION"
+    )
+    devkit_generic.utils.text.echo_pass.to.text.to.stderr.sh "starting :: $VM_ID"
+    sleep 5
+  fi
+
+done
