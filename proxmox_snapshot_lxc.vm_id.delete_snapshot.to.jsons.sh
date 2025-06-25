69b82f090f91e5ae120bc077d4723b590152503b
Benjamin Collas <@hyde-repo>
2025-06-25 18:08:16 +0200

    feat: (#53) - delete lxc snapshot

---
+++ proxmox_snapshot_lxc.vm_id.delete_snapshot.to.jsons.sh
@@ -0,0 +1,174 @@
+#!/bin/bash
+
+#
+# PR-53
+#
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+set -euo pipefail
+ACTION="snapshot_lxc_delete"
+DEFAULT_OUTPUT_JSON=true
+# ARG_VM_SNAPSHOT_NAME=""
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+show_example() {
+  echo "  :: WITH VALUES FROM STDIN (as plain text) "
+  echo
+  echo "    echo \"100\" | $(basename "$0") "
+  echo "    echo \"101\" | $(basename "$0") --json"
+  echo "    echo \"102\" | $(basename "$0") --text"
+  echo
+  echo "    cat /tmp/vm_id.text | $(basename "$0")"
+  echo
+
+  echo "  :: WITH VALUEs FROM STDIN (as JSON lines)"
+  echo
+
+  local STDIN_JSON_DATA=(
+    '{"vm_id":100}'
+    '{"proxmox_node":"px-testing", "vm_id":100}'
+    '{"proxmox_node":"px-testing", "vm_id":100, "lxc_snapshot_name":"MY_LXC_SNAPSHOT"}'
+  )
+
+  for json in "${STDIN_JSON_DATA[@]}"; do
+    devkit_utils.text.echo_json_helper.to.text.sh "$json"
+  done | sed '$ s/$/ | '"$(basename "$0")"'/'
+
+  printf '%s | %s\n' "$(devkit_utils.text.echo_json_helper.to.text.sh "${STDIN_JSON_DATA[-1]}")" "$(basename "$0") "
+
+  echo ""
+  echo "    cat /tmp/lxc_list.json | $(basename "$0")"
+  echo ""
+  echo "    proxmox_lxc.list.to.jsons.sh          | jq -r '.vm_id' | $(basename "$0")"
+  echo "    proxmox_lxc.list.to.jsons.sh group_02 | jq -r '.vm_id' | $(basename "$0")"
+}
+
+if [ "${1-}" = '-h' ] || [ "${1-}" = '--help' ]; then
+  echo
+  echo
+  echo NAME
+  echo
+  echo
+  echo "  $(basename "$0") - delete LXC snapshot - require vm_id  - Execute the specified $ACTION action via Ansible "
+  echo
+  echo OPTIONS
+  echo
+  echo "  $(basename "$0") [-h|--help] "
+  echo "  STDIN :: [VM_ID] | $(basename "$0")  [--json]                                     - force output as json *default"
+
+  echo "  STDIN :: [VM_ID] | $(basename "$0")  [--text]                                     - force output as text"
+  echo ""
+
+  echo EXAMPLE
+  echo "$(show_example)"
+  echo
+  echo
+  exit 1
+fi
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+proxmox__inc.warmup_checks.sh
+proxmox__inc.warmup_checks_stdin.sh
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#
+# define output type
+#
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+OUTPUT_JSON="$DEFAULT_OUTPUT_JSON"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  --json)
+    OUTPUT_JSON=true
+    shift
+    ;;
+  --text)
+    OUTPUT_JSON=false
+    shift
+    ;;
+  -*)
+    devkit_utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+    show_example
+    exit 1
+    ;;
+  *) ;;
+    # if [[ -z "$ARG_VM_SNAPSHOT_NAME" ]]; then
+    #   ARG_VM_SNAPSHOT_NAME="$1"
+    #   shift
+    # else
+    #   devkit_utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+    #   show_example
+    #   exit 1
+    # fi
+    # ;;
+  esac
+done
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+JSON_LINE_REQ=$(devkit_proxmox.STDIN.stdin_or_jsons.to.jsons.sh "INT::vm_id" "STR::lxc_snapshot_name" "STR::lxc_snapshot_description" "STR::lxc_name" "STR::proxmox_node" "STR::action")
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+printf '%s\n' "$JSON_LINE_REQ" | while IFS=$'\n' read -r CURRENT_JSON_LINE; do
+
+  IS_LXC_SNAPSHOT_NAME=$(jq -r '.lxc_snapshot_name // empty' <<<"$CURRENT_JSON_LINE")
+
+  C_VM_NAME=$(printf '%s\n' "$CURRENT_JSON_LINE" | proxmox_lxc.vm_id.list_lxc_and_extract_vm_name.to.jsons.sh | jq -c -r ".lxc_name")
+
+  devkit_utils.text.echo_error.to.text.to.stderr.sh "C_VM_NAME $C_VM_NAME"
+
+  if [[ -z "$IS_LXC_SNAPSHOT_NAME" ]]; then # no associated vm_id ?
+
+    C_LXC_LAST_SNAPSHOT_NAME=$(printf '%s\n' "$CURRENT_JSON_LINE" |
+      proxmox_snapshot_lxc.vm_id.list_snapshot.to.jsons.sh |
+      jq -s -r -c ".[-1].lxc_snapshot_parent")
+
+    if [[ $C_LXC_LAST_SNAPSHOT_NAME == "?" ]]; then
+      devkit_utils.text.echo_error.to.text.to.stderr.sh "NOTHING TO DELETE - NO SNAPSHOT FOUND"
+      exit 1
+    fi
+
+    NEW_CURRENT_JSON_LINE=$(
+      printf '%s\n' "$CURRENT_JSON_LINE" |
+        jq -c \
+          --arg jq_lxc_snapshot_name_v "$C_LXC_LAST_SNAPSHOT_NAME" \
+          --arg jq_lxc_name_v "$C_VM_NAME" \
+          '
+          . + {
+                ("lxc_name"): $jq_lxc_name_v,
+                ("lxc_snapshot_name"): $jq_lxc_snapshot_name_v,
+              }
+          '
+    )
+  else
+
+    NEW_CURRENT_JSON_LINE=$(
+      printf '%s\n' "$CURRENT_JSON_LINE" |
+        jq -c --arg jq_lxc_name_v "$C_VM_NAME" ' . + { ("lxc_name"): $jq_lxc_name_v } '
+    )
+  fi
+
+  # # JSON HAS BEEN UPDATED MUST BE UPDATED
+  CURRENT_JSON_LINE=$NEW_CURRENT_JSON_LINE
+  devkit_utils.text.echo_error.to.text.to.stderr.sh "$CURRENT_JSON_LINE"
+
+  if [[ "$OUTPUT_JSON" == true ]]; then
+
+    printf '%s\n' "$CURRENT_JSON_LINE" |
+      proxmox__inc.jsons.basic_vm_actions.to.jsons.sh "$ACTION" #|
+
+  else
+
+    devkit_utils.text.echo_error.to.text.to.stderr.sh "$CURRENT_JSON_LINE"
+    printf '%s\n' "$CURRENT_JSON_LINE" |
+      proxmox__inc.jsons.basic_vm_actions.to.text.sh "$ACTION"
+
+  fi
+
+done
