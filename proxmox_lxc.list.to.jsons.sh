62ee7f43e5f8a5d615f6ecf94fc7b8b9776ada74
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-06-25 18:07:03 +0200

    feat: (#63) - list lxc container on proxmox node

---
+++ proxmox_lxc.list.to.jsons.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+#
+# PR-63
+#
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+set -euo pipefail
+ACTION="lxc_list"
+DEFAULT_OUTPUT_JSON=true
+ARG_VM_NAME_FILTER=""
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+show_example() {
+  echo "  :: WITH VALUES FROM STDIN (as plain text) "
+  echo
+  echo "    $(basename "$0") "
+  echo "    $(basename "$0") --json"
+  echo "    echo \"px-node-02\" | $(basename "$0") --text"
+  echo
+  echo "    cat /tmp/proxmox_nodes.text | $(basename "$0")"
+  echo
+
+  echo "  :: WITH VALUEs FROM STDIN (as JSON lines)"
+  echo
+
+  local STDIN_JSON_DATA=(
+    '{"proxmox_node":"px-testing"}'
+  )
+
+  for json in "${STDIN_JSON_DATA[@]}"; do
+    devkit_utils.text.echo_json_helper.to.text.sh "$json"
+  done | sed '$ s/$/ | '"$(basename "$0")"'/'
+
+  printf '%s | %s\n' "$(devkit_utils.text.echo_json_helper.to.text.sh "${STDIN_JSON_DATA[-1]}")" "$(basename "$0") vm_test_01 --json"
+  printf '%s | %s\n' "$(devkit_utils.text.echo_json_helper.to.text.sh "${STDIN_JSON_DATA[-1]}")" "$(basename "$0") group_01 --json"
+
+  echo ""
+  echo "    cat /tmp/proxmox_nodes.json | $(basename "$0")"
+}
+
+if [ "${1:-}" = "-h" ] || [ "${1:-}" = "--help" ]; then
+
+  echo
+  echo
+  echo NAME
+  echo "  $(basename "$0") - list LXC status - Execute the specified $ACTION action via Ansible "
+  echo
+  echo OPTION
+  echo
+  echo "  $(basename "$0") [-h|--help] "
+  echo "  $(basename "$0") [--json]                               - force output as json "
+  echo "  $(basename "$0") [partial_or_complete_vm_name] [--json] - Force output in JSON format with a case insensitive filter on vm_name "
+  echo "  $(basename "$0") [--text]                               - force output as text (debug purpose)"
+  echo
+  echo EXAMPLE
+  echo
+  echo "$(show_example)"
+  echo
+  echo
+  exit 1
+fi
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+proxmox__inc.warmup_checks.sh
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#
+# browse provided arugments :
+# - look for output types or filter on vm_name
+#
+
+OUTPUT_JSON="$DEFAULT_OUTPUT_JSON"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  --json)
+    OUTPUT_JSON=true
+    shift
+    ;;
+  --text)
+    OUTPUT_JSON=false
+    shift
+    ;;
+  -*)
+    devkit_utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+    show_example
+    exit 1
+    ;;
+  *)
+    if [[ -z "$ARG_VM_NAME_FILTER" ]]; then
+      ARG_VM_NAME_FILTER="$1"
+      shift
+    else
+      devkit_utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+      show_example
+      exit 1
+    fi
+    ;;
+  esac
+done
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+JSON_LINE_REQ=$(devkit_proxmox.STDIN.stdin_or_jsons.to.jsons.sh "STR::proxmox_node" "STR::action")
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+printf '%s\n' "$JSON_LINE_REQ" | while IFS=$'\n' read -r NODE_JSON; do
+  if [[ "$OUTPUT_JSON" == true ]]; then # json mode.
+
+    if [[ -n "$ARG_VM_NAME_FILTER" ]]; then # check if filter provided in argument
+
+      printf '%s\n' "$NODE_JSON" |
+        proxmox__inc.jsons.basic_vm_actions.to.jsons.sh "$ACTION" |
+        jq '.[]' |
+        devkit_transform.jsons.remove_key.to.jsons.sh "lxc_meta" |
+        devkit_transform.jsons.key_field_greper.to.jsons.sh "lxc_name" "$ARG_VM_NAME_FILTER"
+
+    else # not filter in argument
+
+      # devkit_utils.text.echo_trace.to.text.to.stderr.sh "IN2 $NODE_JSON:: "
+
+      printf '%s\n' "$NODE_JSON" |
+        proxmox__inc.jsons.basic_vm_actions.to.jsons.sh "$ACTION" |
+        jq '.[]' |
+        devkit_transform.jsons.remove_key.to.jsons.sh "lxc_meta"
+
+    fi
+
+  else # text output mode  - debug
+
+    printf '%s\n' "$NODE_JSON" |
+      proxmox__inc.jsons.basic_vm_actions.to.text.sh "$ACTION"
+
+  fi
+
+done
