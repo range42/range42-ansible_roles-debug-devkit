4021a61657e91563145961f718a643061b654fed
Benjamin Collas <129123125+hyde-repo@users.noreply.github.com>
2025-07-01 00:15:43 +0200

    feat:  (#97) - delete iptables alias

---
+++ proxmox_firewall.vm_id.delete_all_iptables_alias.to.jsons.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+#
+# ISSUE -97
+#
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+set -euo pipefail
+ACTION="firewall_vm_delete_iptables_alias"
+DEFAULT_OUTPUT_JSON=true
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+show_example() {
+  echo "  :: WITH VALUES FROM STDIN (as plain text) "
+  echo
+  echo "    echo \"100\" | $(basename "$0") "
+  echo "    echo \"101\" | $(basename "$0") --json"
+  echo "    echo \"102\" | $(basename "$0") --text"
+  echo
+  echo "    cat /tmp/vm_id.text | $(basename "$0")"
+  echo
+
+  echo "  :: WITH VALUEs FROM STDIN (as JSON lines)"
+  echo
+
+  local STDIN_JSON_DATA=(
+    # INPUT
+    '{ "vm_id":100,  "vm_fw_alias_name": "my_alias"}'
+    '{ "vm_id":100, "proxmox_node": "px-testing", "vm_fw_alias_name": "my_alias" }'
+
+  )
+
+  for json in "${STDIN_JSON_DATA[@]}"; do
+    devkit_utils.text.echo_json_helper.to.text.sh "$json"
+  done | sed '$ s/$/ | '"$(basename "$0")"'/'
+
+  printf '%s | %s\n' "$(devkit_utils.text.echo_json_helper.to.text.sh "${STDIN_JSON_DATA[-1]}")" "$(basename "$0") --json"
+
+  echo ""
+
+}
+
+if [ "${1-}" = '-h' ] || [ "${1-}" = '--help' ]; then
+  echo
+  echo
+  echo NAME
+  echo
+  echo "  $(basename "$0") - Delete iptables alias (vm) - Execute the specified $ACTION action via Ansible "
+  echo
+  echo OPTIONS
+  echo
+  echo "                     $(basename "$0") [-h|--help] "
+  echo "  STDIN :: [VM_ID] | $(basename "$0") [--json]    - force output as json *default"
+  echo "  STDIN :: [VM_ID] | $(basename "$0") [--text]    - force output as text"
+  echo ""no
+  echo EXAMPLE
+  echo
+  echo "$(show_example)"
+  echo
+  echo
+  exit 1
+fi
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+proxmox__inc.warmup_checks.sh
+proxmox__inc.warmup_checks_stdin.sh
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+#
+# define output type
+#
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+OUTPUT_JSON="$DEFAULT_OUTPUT_JSON"
+
+case "${1:-}" in
+--json)
+  OUTPUT_JSON=true
+  ;;
+--text)
+  OUTPUT_JSON=false
+  ;;
+"") ;;
+*)
+  devkit_utils.text.echo_error.to.text.to.stderr.sh "wrong number of arguments."
+  show_example
+  exit 1
+  ;;
+esac
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+JSON_LINE_REQ=$(devkit_proxmox.STDIN.stdin_or_jsons.to.jsons.sh "INT::vm_id" "STR::proxmox_node" "STR::vm_name" "STR::action" |
+  proxmox_firewall.vm_id.list_iptables_alias.to.jsons.sh)
+
+#### #### #### #### #### #### #### #### #### #### #### #### #### #### #### ####
+
+printf '%s\n' "$JSON_LINE_REQ" | while IFS=$'\n' read -r CURRENT_JSON_LINE; do
+
+  if [[ "$OUTPUT_JSON" == true ]]; then
+
+    # devkit_utils.text.echo_trace.to.text.to.stderr.sh "$CURRENT_JSON_LINE"
+    # exit 0
+
+    printf '%s\n' "$CURRENT_JSON_LINE" |
+      jq -c ' {proxmox_node, vm_id, vm_fw_alias_name }' |
+      proxmox__inc.jsons.basic_vm_actions.to.jsons.sh "$ACTION"
+    # |
+    # jq -c ".[]"
+
+  else
+
+    printf '%s\n' "$CURRENT_JSON_LINE" |
+      jq -c ' {proxmox_node, vm_id, vm_fw_alias_name }' |
+      proxmox__inc.jsons.basic_vm_actions.to.text.sh "$ACTION"
+
+  fi
+
+done
